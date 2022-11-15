51f1142c316db3f933b9273757e2adb7673d0f03
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-11-15 14:08:44 -0500

    Adding login and register to views and navbar

--- Gifter/client/src/components/ApplicationViews.js
+++ Gifter/client/src/components/ApplicationViews.js
@@ -3,16 +3,22 @@ import { Routes, Route } from "react-router-dom";
 import PostList from "./PostList";
 import PostForm from "./PostForm";
 import PostDetails from "./PostDetails";
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 
 const ApplicationViews = () => {
   return (
     <Routes>
+      <Route path="/login" element= {<Login />} />
+
+      <Route path="/register" element= {<Register />} />
+
       <Route path="/" element= {<PostList />} />
 
       <Route path="/posts/add" element={<PostForm />} />
 
-      <Route path="/posts/:id" element={< PostDetails />} />
+      <Route path="/posts/:id" element={<PostDetails />} />
 
       <Route path="*" element={<p>Whoops, nothing here...</p>} />
     </Routes>
--- Gifter/client/src/components/Header.js
+++ Gifter/client/src/components/Header.js
@@ -18,6 +18,16 @@ const Header = () => {
             New Post
           </Link>
         </li>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            Register
+          </Link>
+        </li>
       </ul>
     </nav>
   );
